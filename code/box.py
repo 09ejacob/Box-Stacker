03d3987e42817ac727938dbeeadbaeea7d76abd0
Garjac <elias.garseth.jacobsen@gmail.com>
2025-05-26 17:38:17 +0200

    Make the box size parameter format width, length, height. Small changes to stacker as well

--- code/box.py
+++ code/box.py
@@ -1,69 +1,64 @@
 import pyglet
 from pyglet.gl import (
-    glViewport, glMatrixMode, glLoadIdentity,
-    glEnable, glBlendFunc, glLightfv, glColor4f, glNormal3f,
-    glTranslatef, glRotatef, glPushMatrix, glPopMatrix,
-    glScalef, glBegin, glEnd, glVertex3f,
-    GL_PROJECTION, GL_MODELVIEW, GL_DEPTH_TEST, GL_BLEND,
-    GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA,
-    GL_LIGHTING, GL_LIGHT0, GL_POSITION,
-    GL_QUADS, gluPerspective
+    glEnable, glBlendFunc, glColor4f, glNormal3f,
+    glPushMatrix, glTranslatef, glScalef, glBegin, glVertex3f, glEnd, glPopMatrix,
+    GL_QUADS, GL_DEPTH_TEST, GL_BLEND, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA
 )
 
 class Box:
     def __init__(self, center, size, color=(1,1,1,0.5)):
         self.x, self.y, self.z = center
-        self.w, self.h, self.d = size
+        self.w, self.l, self.h = size
         self.r, self.g, self.b, self.a = color
 
     def draw(self):
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
         glColor4f(self.r, self.g, self.b, self.a)
         glPushMatrix()
         glTranslatef(self.x, self.y, self.z)
-        glScalef(self.w, self.h, self.d)
+
+        glScalef(self.w, self.h, self.l)
+
         glBegin(GL_QUADS)
-
-        # front
+        # front  (+Z)
         glNormal3f(0, 0, 1)
-        glVertex3f(-.5, -.5,  .5)
-        glVertex3f( .5, -.5,  .5)
-        glVertex3f( .5,  .5,  .5)
-        glVertex3f(-.5,  .5,  .5)
-
-        # back
+        glVertex3f(-0.5, -0.5,  0.5)
+        glVertex3f( 0.5, -0.5,  0.5)
+        glVertex3f( 0.5,  0.5,  0.5)
+        glVertex3f(-0.5,  0.5,  0.5)
+        # back   (–Z)
         glNormal3f(0, 0, -1)
-        glVertex3f(-.5, -.5, -.5)
-        glVertex3f(-.5,  .5, -.5)
-        glVertex3f( .5,  .5, -.5)
-        glVertex3f( .5, -.5, -.5)
-
-        # left
+        glVertex3f(-0.5, -0.5, -0.5)
+        glVertex3f(-0.5,  0.5, -0.5)
+        glVertex3f( 0.5,  0.5, -0.5)
+        glVertex3f( 0.5, -0.5, -0.5)
+        # left   (–X)
         glNormal3f(-1, 0, 0)
-        glVertex3f(-.5, -.5, -.5)
-        glVertex3f(-.5, -.5,  .5)
-        glVertex3f(-.5,  .5,  .5)
-        glVertex3f(-.5,  .5, -.5)
-
-        # right
+        glVertex3f(-0.5, -0.5, -0.5)
+        glVertex3f(-0.5, -0.5,  0.5)
+        glVertex3f(-0.5,  0.5,  0.5)
+        glVertex3f(-0.5,  0.5, -0.5)
+        # right  (+X)
         glNormal3f(1, 0, 0)
-        glVertex3f( .5, -.5, -.5)
-        glVertex3f( .5,  .5, -.5)
-        glVertex3f( .5,  .5,  .5)
-        glVertex3f( .5, -.5,  .5)
-
-        # top
+        glVertex3f( 0.5, -0.5, -0.5)
+        glVertex3f( 0.5,  0.5, -0.5)
+        glVertex3f( 0.5,  0.5,  0.5)
+        glVertex3f( 0.5, -0.5,  0.5)
+        # top    (+Y)
         glNormal3f(0, 1, 0)
-        glVertex3f(-.5,  .5, -.5)
-        glVertex3f(-.5,  .5,  .5)
-        glVertex3f( .5,  .5,  .5)
-        glVertex3f( .5,  .5, -.5)
-        
-        # bottom
+        glVertex3f(-0.5,  0.5, -0.5)
+        glVertex3f(-0.5,  0.5,  0.5)
+        glVertex3f( 0.5,  0.5,  0.5)
+        glVertex3f( 0.5,  0.5, -0.5)
+        # bottom (–Y)
         glNormal3f(0, -1, 0)
-        glVertex3f(-.5, -.5, -.5)
-        glVertex3f( .5, -.5, -.5)
-        glVertex3f( .5, -.5,  .5)
-        glVertex3f(-.5, -.5,  .5)
-        
+        glVertex3f(-0.5, -0.5, -0.5)
+        glVertex3f( 0.5, -0.5, -0.5)
+        glVertex3f( 0.5, -0.5,  0.5)
+        glVertex3f(-0.5, -0.5,  0.5)
         glEnd()
+
         glPopMatrix()
--- code/stacker.py
+++ code/stacker.py
@@ -1,39 +1,71 @@
-# stacker.py
 import json
 import random
 from box import Box
 
 class Stacker:
-    def __init__(self, pallet_size=(0.8, 0.144, 1.2), pallet_color=(0.6,0.6,0.6,1.0), spacing=0.01):
-        px, ph, pz = pallet_size
-        self.boxes = [ Box((0, ph/2, 0), pallet_size, pallet_color) ]
-        self.spacing = spacing
-        self.items = []
+    def __init__(self,
+                 pallet_size=(0.8, 1.2, 0.144),
+                 pallet_color=(0.6, 0.6, 0.6, 1.0)):
+        self.pallet_size = pallet_size
+        pw, pl, ph = pallet_size
+
+        self.boxes = [Box((0, ph/2, 0), pallet_size, pallet_color)]
+        self.items  = []
 
     def load_from_file(self, path):
         with open(path, 'r') as f:
             self.items = json.load(f)
 
     def stack(self):
-        pallet = self.boxes[0]
-        pw, ph, pd = pallet.w, pallet.h, pallet.d
-        base_y = ph
+        pw, pl, ph = self.pallet_size
+        todo = []
+        for it in self.items:
+            w, l, h = it['size']
+            cnt = it.get('count', 1)
+            todo += [(w, l, h)] * cnt
 
-        for item in self.items:
-            w, h, d = item['size']
-            count = item.get('count', 1)
+        todo.sort(key=lambda b: b[0] * b[1], reverse=True)
 
-            per_x = max(int((pw + self.spacing) // (w + self.spacing)), 1)
-            per_z = max(int((pd + self.spacing) // (d + self.spacing)), 1)
-            puts = min(count, per_x * per_z)
+        layer_y   = ph
+        remaining = todo[:]
 
-            for i in range(puts):
-                ix = i % per_x
-                iz = i // per_x
+        while remaining:
+            free_rects    = [(-pw/2, -pl/2, pw, pl)]
+            placed_heights = []
 
-                x = -pw/2 + w/2 + ix * (w + self.spacing)
-                z = -pd/2 + d/2 + iz * (d + self.spacing)
-                y = base_y + h/2
+            while True:
+                best = None
+                for idx, (w, l, h) in enumerate(remaining):
+                    for rect in free_rects:
+                        x0, z0, rw, rl = rect
+                        if w <= rw and l <= rl:
+                            leftover = (rw*rl) - (w*l)
+                            if best is None or leftover < best[0]:
+                                best = (leftover, idx, rect, w, l, h)
+                if not best:
+                    break
 
-                color = (random.random(), random.random(), random.random(), 0.8)
-                self.boxes.append(Box((x, y, z), (w, h, d), color))
+                _, idx, rect, w, l, h = best
+                x0, z0, rw, rl = rect
+
+                x = x0 + w/2
+                z = z0 + l/2
+                y = layer_y + h/2
+
+                color = (random.random(), random.random(), random.random(), 0.5)
+                self.boxes.append(Box((x, y, z), (w, l, h), color))
+                placed_heights.append(h)
+
+                free_rects.remove(rect)
+                right = (x0 + w, z0,      rw - w, l)
+                top   = (x0,     z0 + l,  rw,     rl - l)
+                for nr in (right, top):
+                    if nr[2] > 1e-6 and nr[3] > 1e-6:
+                        free_rects.append(nr)
+
+                remaining.pop(idx)
+
+            if not placed_heights:
+                break
+
+            layer_y += max(placed_heights)
