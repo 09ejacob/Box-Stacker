acdf912ddcf836fd3af4888ce863f0fd16db037e
09ejacob <elias.garseth.jacobsen@gmail.com>
2025-05-26 13:00:33 +0200

    Refactor a bit and create a default pallet object. Also add a light.

---
+++ box.py
@@ -0,0 +1,62 @@
+import pyglet
+from pyglet.gl import (
+    glViewport, glMatrixMode, glLoadIdentity,
+    glEnable, glBlendFunc, glLightfv, glColor4f, glNormal3f,
+    glTranslatef, glRotatef, glPushMatrix, glPopMatrix,
+    glScalef, glBegin, glEnd, glVertex3f,
+    GL_PROJECTION, GL_MODELVIEW, GL_DEPTH_TEST, GL_BLEND,
+    GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA,
+    GL_LIGHTING, GL_LIGHT0, GL_POSITION,
+    GL_QUADS, gluPerspective
+)
+
+class Box:
+    def __init__(self, center, size, color=(1,1,1,0.5)):
+        self.x, self.y, self.z = center
+        self.w, self.h, self.d = size
+        self.r, self.g, self.b, self.a = color
+
+    def draw(self):
+        glColor4f(self.r, self.g, self.b, self.a)
+        glPushMatrix()
+        glTranslatef(self.x, self.y, self.z)
+        glScalef(self.w, self.h, self.d)
+        glBegin(GL_QUADS)
+        # front
+        glNormal3f(0, 0, 1)
+        glVertex3f(-.5, -.5,  .5)
+        glVertex3f( .5, -.5,  .5)
+        glVertex3f( .5,  .5,  .5)
+        glVertex3f(-.5,  .5,  .5)
+        # back
+        glNormal3f(0, 0, -1)
+        glVertex3f(-.5, -.5, -.5)
+        glVertex3f(-.5,  .5, -.5)
+        glVertex3f( .5,  .5, -.5)
+        glVertex3f( .5, -.5, -.5)
+        # left
+        glNormal3f(-1, 0, 0)
+        glVertex3f(-.5, -.5, -.5)
+        glVertex3f(-.5, -.5,  .5)
+        glVertex3f(-.5,  .5,  .5)
+        glVertex3f(-.5,  .5, -.5)
+        # right
+        glNormal3f(1, 0, 0)
+        glVertex3f( .5, -.5, -.5)
+        glVertex3f( .5,  .5, -.5)
+        glVertex3f( .5,  .5,  .5)
+        glVertex3f( .5, -.5,  .5)
+        # top
+        glNormal3f(0, 1, 0)
+        glVertex3f(-.5,  .5, -.5)
+        glVertex3f(-.5,  .5,  .5)
+        glVertex3f( .5,  .5,  .5)
+        glVertex3f( .5,  .5, -.5)
+        # bottom
+        glNormal3f(0, -1, 0)
+        glVertex3f(-.5, -.5, -.5)
+        glVertex3f( .5, -.5, -.5)
+        glVertex3f( .5, -.5,  .5)
+        glVertex3f(-.5, -.5,  .5)
+        glEnd()
+        glPopMatrix()
--- viewer.py
+++ viewer.py
@@ -1,82 +1,29 @@
 import pyglet
+from pyglet.gl import GLfloat
 from pyglet.gl import (
     glViewport, glMatrixMode, glLoadIdentity,
-    glEnable, glTranslatef, glRotatef, glPushMatrix, glPopMatrix,
+    glEnable, glBlendFunc, glLightfv, glColor4f, glNormal3f,
+    glTranslatef, glRotatef, glPushMatrix, glPopMatrix,
     glScalef, glBegin, glEnd, glVertex3f,
-    GL_PROJECTION, GL_MODELVIEW, GL_DEPTH_TEST, GL_QUADS,
-    gluPerspective
+    GL_PROJECTION, GL_MODELVIEW, GL_DEPTH_TEST, GL_BLEND,
+    GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA,
+    GL_LIGHTING, GL_LIGHT0, GL_POSITION,
+    GL_QUADS, gluPerspective
 )
-
-# --- define a Box by center + size, and draw with GL_QUADS ---
-class Box:
-    def __init__(self, center, size):
-        self.x, self.y, self.z = center
-        self.w, self.h, self.d = size
-
-    def draw(self):
-        glPushMatrix()
-        glTranslatef(self.x, self.y, self.z)
-        glScalef(self.w, self.h, self.d)
-        glBegin(GL_QUADS)
-
-        # front
-        glVertex3f(-0.5, -0.5,  0.5)
-        glVertex3f( 0.5, -0.5,  0.5)
-        glVertex3f( 0.5,  0.5,  0.5)
-        glVertex3f(-0.5,  0.5,  0.5)
-
-        # back
-        glVertex3f(-0.5, -0.5, -0.5)
-        glVertex3f(-0.5,  0.5, -0.5)
-        glVertex3f( 0.5,  0.5, -0.5)
-        glVertex3f( 0.5, -0.5, -0.5)
-
-        # left
-        glVertex3f(-0.5, -0.5, -0.5)
-        glVertex3f(-0.5, -0.5,  0.5)
-        glVertex3f(-0.5,  0.5,  0.5)
-        glVertex3f(-0.5,  0.5, -0.5)
-
-        # right
-        glVertex3f( 0.5, -0.5, -0.5)
-        glVertex3f( 0.5,  0.5, -0.5)
-        glVertex3f( 0.5,  0.5,  0.5)
-        glVertex3f( 0.5, -0.5,  0.5)
-
-        # top
-        glVertex3f(-0.5,  0.5, -0.5)
-        glVertex3f(-0.5,  0.5,  0.5)
-        glVertex3f( 0.5,  0.5,  0.5)
-        glVertex3f( 0.5,  0.5, -0.5)
-
-        # bottom
-        glVertex3f(-0.5, -0.5, -0.5)
-        glVertex3f( 0.5, -0.5, -0.5)
-        glVertex3f( 0.5, -0.5,  0.5)
-        glVertex3f(-0.5, -0.5,  0.5)
-        
-        glEnd()
-        glPopMatrix()
+from box import Box
 
 boxes = [
-    Box(center=(0, 0, 0),   size=(1, 1, 1)),
-    Box(center=(2, 0, 0),   size=(0.5, 2, 0.5)),
-    Box(center=(0, 2, -1),  size=(1, 0.5, 2)),
+    Box((0,0,0), (0.8,0.144,1.2), (1,0,0,0.6)),
 ]
 
-# --- window + camera state ---
 window = pyglet.window.Window(800, 600, "3D view", resizable=True)
-yaw = 30
-pitch = -20
-dist = 8.0
-pan_x = pan_y = 0.0
+yaw = 30; pitch = -20; dist = 8.0; pan_x = pan_y = 0.0
 
 @window.event
 def on_resize(width, height):
     glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(60.0, width/float(height), 0.1, 1000.0)
+    glMatrixMode(GL_PROJECTION); glLoadIdentity()
+    gluPerspective(20.0, width/float(height), 0.1, 1000.0)
     glMatrixMode(GL_MODELVIEW)
     return True
 
@@ -84,11 +31,12 @@ def on_resize(width, height):
 def on_draw():
     window.clear()
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glEnable(GL_LIGHTING); glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, (GLfloat * 4)(1.0, 1.0, 1.0, 0.0))
     glLoadIdentity()
-
     glTranslatef(pan_x, pan_y, -dist)
-    glRotatef(-pitch, 1, 0, 0)
-    glRotatef(yaw,   0, 1, 0)
+    glRotatef(-pitch, 1, 0, 0); glRotatef(yaw, 0, 1, 0)
 
     for b in boxes:
         b.draw()
@@ -96,19 +44,14 @@ def on_draw():
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, mods):
     global yaw, pitch, pan_x, pan_y
-
     if buttons & pyglet.window.mouse.LEFT:
-        yaw   += dx * 0.3
-        pitch += dy * 0.3
-
-    elif buttons & (pyglet.window.mouse.RIGHT | pyglet.window.mouse.MIDDLE):
-        pan_x += dx * (dist / window.width)
-        pan_y += dy * (dist / window.height)
+        yaw   += dx * 0.3; pitch += dy * 0.3
+    else:
+        pan_x += dx * (dist/window.width); pan_y += dy * (dist/window.height)
 
 @window.event
-def on_mouse_scroll(x, y, scroll_x, scroll_y):
-    global dist
-    dist = max(1.0, dist - scroll_y * 0.5)
+def on_mouse_scroll(x, y, sx, sy):
+    global dist; dist = max(1.0, dist - sy * 0.5)
 
 if __name__ == '__main__':
     pyglet.app.run()
