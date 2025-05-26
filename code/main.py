e3e72b208549ced3e6695c6e1b46115d4f6fe6a3
Garjac <elias.garseth.jacobsen@gmail.com>
2025-05-26 13:52:01 +0200

    Add small changes in the boxes.json and in main

--- code/main.py
+++ code/main.py
@@ -1,12 +1,13 @@
-# main.py
 import pyglet
 import viewer
 from stacker import Stacker
 
-# main.py
 if __name__ == '__main__':
     st = Stacker()
-    st.load_from_file('boxes/boxes.json')   # <-- no “..”
+    
+    st.load_from_file('boxes/boxes.json')
     st.stack()
+    
     viewer.boxes = st.boxes
+
     pyglet.app.run()
